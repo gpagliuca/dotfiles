9ccee01184db05ed7c0ed3708d5f0e5fdc9ec757
peppe <peppe@pop-os.localdomain>
2025-04-25 00:51:07 +0200

    added startship

--- install_script.sh
+++ install_script.sh
@@ -1,3 +1,7 @@
+#----- Append to PATH
+LINE0='export PATH=$PATH:/home/$USER/.local/bin'
+grep -qF -- "$LINE0" ~/.bashrc || echo "$LINE0" >>~/.bashrc
+
 #----- Tmux
 rm -rf ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -20,10 +24,8 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 #----- zoxide
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
-LINE0='export PATH=$PATH:/home/$USER/.local/bin'
-grep -qF -- "$LINE0" ~/.bashrc || echo "$LINE0" >>~/.bashrc
-LINE1='eval "$(zoxide init bash)"'
-grep -qF -- "$LINE1" ~/.bashrc || echo "$LINE1" >>~/.bashrc
+LINE='eval "$(zoxide init bash)"'
+grep -qF -- "$LINE" ~/.bashrc || echo "$LINE" >>~/.bashrc
 
 #----- NVIM from scratch
 rm -rf ~/.config/nvim
@@ -37,5 +39,10 @@ cp -r ~/dotfiles/nvim/ ~/.config/
 
 #----- Aliases
 cp ~/dotfiles/bash_aliases/bash_aliases ~/.bash_aliases
+
+#----- Starship
+curl -sS https://starship.rs/install.sh | sh -s -- -b ~/.local/bin
+LINE='eval "$(zoxide init bash)"'
+grep -qF -- "$LINE" ~/.bashrc || echo "$LINE" >>~/.bashrc
 
 source ~/.bashrc
